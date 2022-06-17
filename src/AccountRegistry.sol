f273716bf0830162213760d62c5cc9c0cd686c4c
Varun Srinivasan <varunsrin@gmail.com>
2022-06-16 22:23:56 -0700

    feat: implement setRecovery for AccountRegistry

--- src/AccountRegistry.sol
+++ src/AccountRegistry.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.14;
 
 error AccountHasId();
+error AccountHasRecovery();
+error AddressNotOwner();
+error InvalidId();
+error CustodyRecoveryDuplicate();
 
 contract AccountRegistry {
     uint256 idCounter;
@@ -11,14 +15,84 @@ contract AccountRegistry {
     function register() public {
         if (custodyAddressToid[msg.sender] != 0) revert AccountHasId();
 
-        // Incrementing before assigning ensures that no one can claim
-        // the 0 ID
-        idCounter++;
+        // Incrementing before assigning ensures that no one can claim the 0 ID. Performing this
+        // unchecked also saves us about 100 GAS and is safe to do given the very large size of
+        // uint256. Should audit this carefully and ensure that the overflow scenario can be
+        // managed.
+        unchecked {
+            idCounter++;
+        }
 
         // Optimization: Do you need to store the uint256 here? can you
         // save gas by using a truthy value and relying on the event
         // for the actual ID?
         custodyAddressToid[msg.sender] = idCounter;
         emit Register(idCounter, msg.sender);
+    }
+
+    /*
+     * Recovery
+     */
+
+    mapping(uint256 => address) public idToRecoveryAddress;
+    mapping(uint256 => uint256) public idToTransferClock;
+    mapping(uint256 => uint256) public idToTransferDestination;
+
+    event SetRecovery(address indexed recovery, uint256 indexed id);
+
+    event TransferRequested(
+        address indexed to,
+        uint256 indexed id,
+        address indexed by,
+        uint256 nonce
+    );
+
+    event TransferCompleted(
+        address indexed to,
+        uint256 indexed id,
+        address indexed performedBy,
+        address approvedBy
+    );
+
+    function setRecovery(uint256 id, address newRecoveryAddress) public {
+        if (id == 0) revert InvalidId();
+        if (custodyAddressToid[msg.sender] != id) revert AddressNotOwner();
+        if (idToRecoveryAddress[id] != address(0)) revert AccountHasRecovery();
+        if (newRecoveryAddress == msg.sender) revert CustodyRecoveryDuplicate();
+        idToRecoveryAddress[id] = newRecoveryAddress;
+        emit SetRecovery(newRecoveryAddress, id);
+    }
+
+    function requestTransfer(uint256 id, address to) public {
+        // check msg.sender == idToRecoveryAddress[id] || idToCustodyAddress[id]
+        // assert to != msg.sender
+        // assert to != idToRecoveryAddress[id]
+        // if (!idToRecoveryAddress[id]) { _transfer(id, to) }
+        // else....
+        // set idToTransferDestination[id] == to
+        // emit TransferRequested
+    }
+
+    // A transfer can be cancelled by either party 1hr after it is created.
+    function cancelTransfer(uint256 id, address to) public {
+        // check msg.sender == idToRecoveryAddress[id] || idToCustodyAddress[id]
+        // assert idToTransferClock[id] != 0
+        // assert blockHeight - idToTransferClock[id] > (258 blocks, ~1hr)
+        // set idToTransferClock[id] = 0;
+        // emit TransferCancelled();
+    }
+
+    function completeTransfer(uint256 id) public {
+        // assert msg.sender == uuidToRecoveryAddress[id] || uuidToCustodyAddress[id]
+        // assert idToTransferClock[id] > 0;
+        // assert blockHeight - idToTransferClock[id] > (19_000 blocks, ~ 3d, 2h)
+        // _transfer(id, uuidToTransferDestination[id]);
+    }
+
+    function _transfer(uint256 id, address to) public {
+        // set uuidToCustodyAddress[id] = to
+        // set idToTransferClock[id] = 0;
+        // reset the recovery address
+        // emit TransferCompleted()
     }
 }
--- test/AccountRegistry.t.sol
+++ test/AccountRegistry.t.sol
@@ -8,6 +8,7 @@ contract AccountRegistryTest is Test {
     AccountRegistry accountRegistry;
 
     event Register(uint256 indexed id, address indexed custodyAddress);
+    event SetRecovery(address indexed recovery, uint256 indexed id);
 
     function setUp() public {
         accountRegistry = new AccountRegistry();
@@ -40,5 +41,61 @@ contract AccountRegistryTest is Test {
         assertEq(accountRegistry.custodyAddressToid(address(this)), 1);
         vm.expectRevert(AccountHasId.selector);
         accountRegistry.register();
+    }
+
+    function testSetRecovery() public {
+        // register id i
+        accountRegistry.register();
+
+        // setRecovery and expect event and recovery to be populated
+        vm.expectEmit(true, true, false, false);
+        emit SetRecovery(address(10), 1);
+        accountRegistry.setRecovery(1, address(10));
+        assertEq(accountRegistry.idToRecoveryAddress(1), address(10));
+    }
+
+    function testCannotSetRecoveryToCustody() public {
+        // register id i
+        accountRegistry.register();
+
+        // setRecovery and expect event and recovery to be populated
+        vm.expectRevert(CustodyRecoveryDuplicate.selector);
+        accountRegistry.setRecovery(1, address(this));
+        assertEq(accountRegistry.idToRecoveryAddress(1), address(0));
+    }
+
+    function testCannotSetRecoveryUnlessOwner() public {
+        // register id 1
+        accountRegistry.register();
+
+        // setRecovery for an unregistered id
+        vm.expectRevert(AddressNotOwner.selector);
+        accountRegistry.setRecovery(2, address(10));
+        assertEq(accountRegistry.idToRecoveryAddress(2), address(0));
+
+        // setRecovery for id 1 from an address that does not own it
+        vm.prank(address(0));
+        vm.expectRevert(AddressNotOwner.selector);
+        accountRegistry.setRecovery(1, address(2));
+        assertEq(accountRegistry.idToRecoveryAddress(1), address(0));
+    }
+
+    function testCannotSetRecoveryTwice() public {
+        // register and setRecovery once
+        accountRegistry.register();
+        accountRegistry.setRecovery(1, address(10));
+        assertEq(accountRegistry.idToRecoveryAddress(1), address(10));
+
+        // setRecovery again and expect it to fail
+        vm.expectRevert(AccountHasRecovery.selector);
+        accountRegistry.setRecovery(1, address(11));
+        assertEq(accountRegistry.idToRecoveryAddress(1), address(10));
+    }
+
+    function testCannotSetRecoveryForZeroId() public {
+        accountRegistry.register();
+        vm.expectRevert(InvalidId.selector);
+        accountRegistry.setRecovery(0, address(10));
+        assertEq(accountRegistry.idToRecoveryAddress(0), address(0));
     }
 }
