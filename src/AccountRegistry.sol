6a4318a0a1c37236b7a17d142363e30651af20ff
Varun Srinivasan <varunsrin@gmail.com>
2022-07-06 21:47:21 -0700

    upgrade: solidity to 0.8.15

--- src/AccountRegistry.sol
+++ src/AccountRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.15;
 
 error Unauthorized(); // The caller does not have the authority to perform this action.
 error ZeroId(); // The id is zero, which is invalid
