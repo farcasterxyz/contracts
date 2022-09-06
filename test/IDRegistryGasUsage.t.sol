9a25433aac380e42fecb76c332a8fc6a2ceea5e6
Varun Srinivasan <varunsrin@gmail.com>
2022-09-05 21:08:52 -0700

    test(IDRegistry): add gas benchmarks for recovery actions (#101)
    * test(IDRegistry): add gas benchmarks for recovery actions
    
    * chore: update snapshots

--- test/IDRegistryGasUsage.t.sol
+++ test/IDRegistryGasUsage.t.sol
@@ -12,18 +12,42 @@ contract IDRegistryGasUsageTest is Test {
 
     address constant FORWARDER = address(0xC8223c8AD514A19Cc10B0C94c39b52D4B43ee61A);
     address constant TRUSTED_SENDER = address(0x123);
+    address constant RECOVERY = address(0x6D1217BD164119E2ddE6ce1723879844FD73114e);
     string url = "https://farcaster.xyz";
 
     function setUp() public {
         idRegistry = new IDRegistryTestable(FORWARDER);
     }
 
-    function testGasRegister() public {
+    function testGasRegisterAndRecover() public {
         idRegistry.disableTrustedOnly();
-        for (uint256 i = 0; i < 25; i++) {
+
+        // Perform each action at least 5 times to get a good median value, since the first action
+        // initializes storage and has extra costs
+
+        for (uint256 i = 0; i < 15; i++) {
+            // Register the name
             address alice = address(uint160(i));
-            idRegistry.register(alice, address(0), url);
+            idRegistry.register(alice, RECOVERY, url);
             assertEq(idRegistry.idOf(address(uint160(i))), i + 1);
+
+            // Requesting a recovery should be done multiple times to get a good median value,
+            // since it adds new storage slots.
+            for (uint256 j = 0; j < 5; j++) {
+                vm.prank(RECOVERY);
+                idRegistry.requestRecovery(alice, RECOVERY);
+            }
+
+            // Cancelling and the recovery can be done once per user since it adds no new storage
+            vm.prank(alice);
+            idRegistry.cancelRecovery(alice);
+
+            vm.prank(RECOVERY);
+            idRegistry.requestRecovery(alice, address(uint160(i + 100)));
+
+            vm.warp(block.timestamp + 7 days);
+            vm.prank(RECOVERY);
+            idRegistry.completeRecovery(alice);
         }
     }
 
