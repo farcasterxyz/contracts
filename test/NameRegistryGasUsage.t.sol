e88870705823a0599273600a66a99c746652ae23
Varun Srinivasan <varunsrin@gmail.com>
2022-09-14 19:52:31 -0700

    test(NameRegistry): refactor gas usage tests to be simpler

--- test/NameRegistryGasUsage.t.sol
+++ test/NameRegistryGasUsage.t.sol
@@ -10,29 +10,31 @@ import {NameRegistry} from "../src/NameRegistry.sol";
 /* solhint-disable state-visibility */
 
 contract NameRegistryGasUsageTest is Test {
+    /// Instance of the implementation contract
     NameRegistry nameRegistryImpl;
-    NameRegistry nameRegistry;
+
+    // Instance of the proxy contract
     ERC1967Proxy nameRegistryProxy;
 
-    address constant POOL = address(0xFe4ECfAAF678A24a6661DB61B573FEf3591bcfD6);
-    address constant VAULT = address(0xec185Fa332C026e2d4Fc101B891B51EFc78D8836);
+    // Instance of the proxy contract cast as the implementation contract
+    NameRegistry nameRegistry;
 
     /*//////////////////////////////////////////////////////////////
                                 CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
     address constant ADMIN = address(0xa6a4daBC320300cd0D38F77A6688C6b4048f4682);
-    address constant TRUSTED_FORWARDER = address(0xC8223c8AD514A19Cc10B0C94c39b52D4B43ee61A);
-    uint256 constant COMMIT_REGISTER_DELAY = 60;
+    address constant POOL = address(0xFe4ECfAAF678A24a6661DB61B573FEf3591bcfD6);
     address constant RECOVERY = address(0x8Ca9aB5b1756B7020a299ff4dc79b5E854a5cac5);
+    address constant TRUSTED_FORWARDER = address(0xC8223c8AD514A19Cc10B0C94c39b52D4B43ee61A);
     address constant TRUSTED_SENDER = address(0x4E29ad5578668e2f82A921FFd5fA7720eDD59D47);
-    uint256 constant RENEWAL_PERIOD = 31 days;
+    address constant VAULT = address(0xec185Fa332C026e2d4Fc101B891B51EFc78D8836);
 
-    uint256 constant DEC1_2022_TS = 1669881600; // Dec 1, 2022 00:00:00 GMT
+    uint256 constant COMMIT_REGISTER_DELAY = 60;
+    uint256 constant RENEWAL_PERIOD = 31 days;
+    uint256 constant REGISTRATION_PERIOD = 365 days;
+
     uint256 constant JAN1_2023_TS = 1672531200; // Jan 1, 2023 0:00:00 GMT
-    uint256 constant JAN1_2024_TS = 1704067200; // Jan 1, 2024 0:00:00 GMT
-    uint256 constant FEB1_2024_TS = 1706745600; // Feb 1, 2024 0:00:00 GMT
-    uint256 constant JAN1_2025_TS = 1735689600; // Jan 1, 2025 0:00:00 GMT
 
     bytes16[10] names = [
         bytes16("alice"),
@@ -56,6 +58,7 @@ contract NameRegistryGasUsageTest is Test {
     function setUp() public {
         nameRegistryImpl = new NameRegistry(TRUSTED_FORWARDER);
         nameRegistryProxy = new ERC1967Proxy(address(nameRegistryImpl), "");
+
         nameRegistry = NameRegistry(address(nameRegistryProxy));
         nameRegistry.initialize("Farcaster NameRegistry", "FCN", VAULT, POOL);
         nameRegistry.grantRole(ADMIN_ROLE, ADMIN);
@@ -65,72 +68,59 @@ contract NameRegistryGasUsageTest is Test {
         vm.prank(ADMIN);
         nameRegistry.disableTrustedOnly();
 
-        // TODO: add more commentary on how these are calculated before merging
-        uint256 commitTs = DEC1_2022_TS;
-        uint256 registerTs = commitTs + COMMIT_REGISTER_DELAY;
-        uint256 renewableTs = registerTs + 365 days;
-        uint256 biddableTs = renewableTs + RENEWAL_PERIOD + 365 days;
-
-        // 1. During 2022, test making the commit and registering the name
         for (uint256 i = 0; i < names.length; i++) {
-            address alice = address(uint160(i) + 10); // start after the precompiles
+            // Add +10 to the integer to skip the precompile contracts
+            address alice = address(uint160(i) + 10);
+            vm.deal(alice, 10_000 ether);
+
+            // Add +100 to avoid collision with alice in all instances
+            address bob = address(uint160(i) + 100);
+
             bytes16 name = names[i];
             uint256 nameTokenId = uint256(bytes32(name));
 
+            // Make the first commit on Jan 1, 2023
+            vm.warp(JAN1_2023_TS);
             bytes32 commitHash = nameRegistry.generateCommit(name, alice, "secret");
-
-            vm.deal(alice, 10_000 ether);
-            vm.warp(commitTs);
 
             vm.prank(alice);
             nameRegistry.makeCommit(commitHash);
             assertEq(nameRegistry.timestampOf(commitHash), block.timestamp);
 
-            // 3. Register the name alice
-            vm.warp(registerTs);
+            // Register the name after the commit register delay has elapsed
+            vm.warp(block.timestamp + COMMIT_REGISTER_DELAY);
             uint256 balance = alice.balance;
             vm.prank(alice);
             nameRegistry.register{value: 0.01 ether}(name, alice, "secret", RECOVERY);
 
-            assertEq(nameRegistry.expiryOf(nameTokenId), renewableTs);
+            uint256 firstExpiration = block.timestamp + REGISTRATION_PERIOD;
+
+            assertEq(nameRegistry.expiryOf(nameTokenId), firstExpiration);
             assertEq(nameRegistry.ownerOf(nameTokenId), alice);
             assertEq(nameRegistry.recoveryOf(nameTokenId), RECOVERY);
             assertEq(alice.balance, balance - nameRegistry.fee());
-        }
 
-        // 2. During 2023, test renewing the name after it expires. This must not be done in the previous
-        // loop since warping forward and triggering the currYear calculation will cause issues if we ever
-        // warp backwards into the previous year.
-        for (uint256 i = 0; i < names.length; i++) {
-            address alice = address(uint160(i) + 10); // start after the precompiles
-            bytes16 name = names[i];
-            uint256 nameTokenId = uint256(bytes32(name));
-            vm.warp(renewableTs);
-
+            // Wait until the registration expires, then renew the registration for a year
+            vm.warp(firstExpiration);
             vm.prank(alice);
             nameRegistry.renew{value: 0.01 ether}(nameTokenId);
 
-            assertEq(nameRegistry.expiryOf(nameTokenId), renewableTs + 365 days);
+            uint256 secondExpiration = block.timestamp + REGISTRATION_PERIOD;
+            assertEq(nameRegistry.expiryOf(nameTokenId), secondExpiration);
             assertEq(nameRegistry.ownerOf(nameTokenId), alice);
-        }
 
-        // 3. During 2024, test bidding on the name after it expires and then transferring it. This is also
-        // done in a separate loop for the same reason as 2
-        for (uint256 i = 0; i < names.length; i++) {
-            address alice = address(uint160(i) + 10); // start after the precompiles
-            address bob = address(uint160(i) + 100);
-            bytes16 name = names[i];
-            uint256 nameTokenId = uint256(bytes32(name));
-            vm.warp(biddableTs);
-
+            // Wait until the second registration expires, then wait for the renewal period to pass
+            // and finally bid on the name
+            vm.warp(secondExpiration + RENEWAL_PERIOD);
             vm.prank(alice);
             nameRegistry.bid{value: 1_000.01 ether}(alice, nameTokenId, RECOVERY);
 
             assertEq(nameRegistry.balanceOf(alice), 1);
-            assertEq(nameRegistry.expiryOf(nameTokenId), block.timestamp + 365 days);
+            assertEq(nameRegistry.expiryOf(nameTokenId), block.timestamp + REGISTRATION_PERIOD);
             assertEq(nameRegistry.ownerOf(nameTokenId), alice);
             assertEq(nameRegistry.recoveryOf(nameTokenId), RECOVERY);
 
+            // Transfer the name to a new owner
             vm.prank(alice);
             nameRegistry.transferFrom(alice, bob, nameTokenId);
 
@@ -153,13 +143,13 @@ contract NameRegistryGasUsageTest is Test {
             uint256 inviteeId = 6;
 
             vm.deal(alice, 10_000 ether);
-            vm.warp(DEC1_2022_TS);
+            vm.warp(JAN1_2023_TS);
 
             vm.prank(TRUSTED_SENDER);
             nameRegistry.trustedRegister(name, alice, RECOVERY, inviterId, inviteeId);
 
             assertEq(nameRegistry.ownerOf(nameTokenId), alice);
-            assertEq(nameRegistry.expiryOf(nameTokenId), block.timestamp + 365 days);
+            assertEq(nameRegistry.expiryOf(nameTokenId), block.timestamp + REGISTRATION_PERIOD);
             assertEq(nameRegistry.recoveryOf(nameTokenId), RECOVERY);
         }
     }
