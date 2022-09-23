dccf70692041b5e56e005e265e0434f5d25795ac
Varun Srinivasan <varunsrin@gmail.com>
2022-09-23 09:49:30 -0700

    feat(BundleRegistry): deploy script configures trusted caller chain

--- script/BundleRegistry.s.sol
+++ script/BundleRegistry.s.sol
@@ -13,17 +13,37 @@ import {NameRegistry} from "../src/NameRegistry.sol";
 
 contract BundleRegistryScript is Script {
     address constant GOERLI_FORWARDER = address(0x7A95fA73250dc53556d264522150A940d4C50238);
-    address constant ADMIN = address(0x973Eff2C8eBC79bECb9937fC42E65D96aCCB3ADd);
+    bytes32 constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    // TODO: Always update this to the address of the private key used to deploy the contracts
+    address constant DEPLOYER = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+
+    // TODO: Update the vault and pool addresses every time
+    address constant VAULT = DEPLOYER;
+    address constant POOL = DEPLOYER;
 
     NameRegistry nameRegistryImpl;
     NameRegistry nameRegistry;
     ERC1967Proxy proxy;
     IdRegistry idRegistry;
+    BundleRegistry bundleRegistry;
 
-    // TODO: Fix the vault and pool address
-    address constant VAULT = ADMIN;
-    address constant POOL = ADMIN;
-
+    /**
+     * @dev Deploys IdRegistry, NameRegistry and BundleRegistry contracts with the following config:
+     *
+     * IdRegistry
+     *  - trusted_caller: DEPLOYER
+     *  - owner         : DEPLOYER
+     *
+     * NameRegistry
+     *  - trusted_caller: BundleRegistry
+     *  - default_admin : DEPLOYER
+     *  - admin         : none
+     *
+     * BundleRegistry
+     *  - trusted_caller: DEPLOYER
+     *  - owner         : DEPLOYER
+     */
     function run() public {
         vm.broadcast();
         idRegistry = new IdRegistry(GOERLI_FORWARDER);
@@ -39,6 +59,19 @@ contract BundleRegistryScript is Script {
         nameRegistry.initialize("Farcaster NameRegistry", "FCN", VAULT, POOL);
 
         vm.broadcast();
-        new BundleRegistry(address(idRegistry), address(nameRegistry), ADMIN);
+        bundleRegistry = new BundleRegistry(address(idRegistry), address(nameRegistry), DEPLOYER);
+
+        // Set the BundleRegistry as the trusted caller for IdRegistry and NameRegistry
+        vm.broadcast();
+        idRegistry.changeTrustedCaller(address(bundleRegistry));
+
+        vm.broadcast();
+        nameRegistry.grantRole(ADMIN_ROLE, DEPLOYER);
+
+        vm.broadcast();
+        nameRegistry.changeTrustedCaller(address(bundleRegistry));
+
+        vm.broadcast();
+        nameRegistry.renounceRole(ADMIN_ROLE, DEPLOYER);
     }
 }
