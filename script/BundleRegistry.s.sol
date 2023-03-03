c9fb1f1e7c681b02b03463db76640e2d725a6ce8
appleseed-iii <appleseed-iii@protonmail.com>
2023-03-02 20:36:38 -0600

    refactor BundleRegistry.s.sol to use PRIVATE_KEY from ENV

--- script/BundleRegistry.s.sol
+++ script/BundleRegistry.s.sol
@@ -15,13 +15,11 @@ contract BundleRegistryScript is Script {
     address constant GOERLI_FORWARDER = address(0x7A95fA73250dc53556d264522150A940d4C50238);
     bytes32 constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    // TODO: Always update this to the address of the private key used to deploy the contracts on the network
-    address constant DEPLOYER = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266); // anvil deployer
+    address DEPLOYER = vm.addr(vm.envUint("PRIVATE_KEY"));
 
     // TODO: Update the vault and pool addresses every time
-    address constant VAULT = DEPLOYER;
-    address constant POOL = DEPLOYER;
-
+    address VAULT = DEPLOYER;
+    address POOL = DEPLOYER;
     NameRegistry nameRegistryImpl;
     NameRegistry nameRegistry;
     ERC1967Proxy proxy;
@@ -45,33 +43,26 @@ contract BundleRegistryScript is Script {
      *  - owner         : DEPLOYER
      */
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(DEPLOYER);
         idRegistry = new IdRegistry(GOERLI_FORWARDER);
 
-        vm.broadcast();
         nameRegistryImpl = new NameRegistry(GOERLI_FORWARDER);
 
-        vm.broadcast();
         proxy = new ERC1967Proxy(address(nameRegistryImpl), "");
         nameRegistry = NameRegistry(address(proxy));
 
-        vm.broadcast();
         nameRegistry.initialize("Farcaster NameRegistry", "FCN", VAULT, POOL);
 
-        vm.broadcast();
         bundleRegistry = new BundleRegistry(address(idRegistry), address(nameRegistry), DEPLOYER);
 
         // Set the BundleRegistry as the trusted caller for IdRegistry and NameRegistry
-        vm.broadcast();
         idRegistry.changeTrustedCaller(address(bundleRegistry));
 
-        vm.broadcast();
         nameRegistry.grantRole(ADMIN_ROLE, DEPLOYER);
 
-        vm.broadcast();
         nameRegistry.changeTrustedCaller(address(bundleRegistry));
 
-        vm.broadcast();
         nameRegistry.renounceRole(ADMIN_ROLE, DEPLOYER);
+        vm.stopBroadcast();
     }
 }
