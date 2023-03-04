695b05251bf17cfcd77f5081d3ca31c1fa6fd763
Varun Srinivasan <varunsrin@gmail.com>
2023-03-04 12:42:30 -0800

    chore: clean up

--- test/IdRegistry.owner.t.sol
+++ test/IdRegistry.owner.t.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
+
 import "./TestConstants.sol";
+import {IdRegistryHarness} from "./Utils.sol";
 
 import {IdRegistry} from "../src/IdRegistry.sol";
-import {IdRegistryHarness} from "./Utils.sol";
 
 /* solhint-disable state-visibility */
 
@@ -24,7 +25,6 @@ contract IdRegistryOwnerTest is Test {
                                 CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
-    address constant FORWARDER = address(0xC8223c8AD514A19Cc10B0C94c39b52D4B43ee61A);
     address owner = address(this);
 
     /*//////////////////////////////////////////////////////////////
