50c2a51b0a707417d386aa92ca00d6c99d5bbfc0
horsefacts <webmaster@terminally.online>
2023-06-27 16:30:53 -0400

    feat: update IdRegistry deploy script

--- script/IdRegistry.s.sol
+++ script/IdRegistry.s.sol
@@ -2,14 +2,25 @@
 pragma solidity 0.8.18;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 
 import {IdRegistry} from "../src/IdRegistry.sol";
 
+contract IdRegistryFab {
+    constructor(address trustedForwarder, address initialOwner, bytes32 salt) {
+        IdRegistry registry = new IdRegistry{ salt: salt }(trustedForwarder);
+        registry.requestTransferOwnership(initialOwner);
+    }
+}
+
 contract IdRegistryScript is Script {
-    address private goerliTrustedForwarder = address(0x7A95fA73250dc53556d264522150A940d4C50238);
+    bytes32 internal constant CREATE2_SALT = "fc";
 
     function run() public {
+        address trustedForwarder = vm.envAddress("ID_REGISTRY_TRUSTED_FORWARDER_ADDRESS");
+        address initialOwner = vm.envAddress("ID_REGISTRY_OWNER_ADDRESS");
+
         vm.broadcast();
-        new IdRegistry(goerliTrustedForwarder);
+        new IdRegistryFab(trustedForwarder, initialOwner, CREATE2_SALT);
     }
 }
