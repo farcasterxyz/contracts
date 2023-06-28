d96416fdd6ba8e60bfc0795a962081484157d9ec
Varun Srinivasan <varunsrin@gmail.com>
2023-06-28 15:38:55 -0700

    chore: update solidity compiler to 0.8.19

--- script/StorageRent.s.sol
+++ script/StorageRent.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
 import {AggregatorV3Interface} from "chainlink/v0.8/interfaces/AggregatorV3Interface.sol";
--- test/StorageRent/StorageRent.t.sol
+++ test/StorageRent/StorageRent.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
