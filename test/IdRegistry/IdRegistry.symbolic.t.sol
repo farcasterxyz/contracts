e50bc79cf1b58da1a39593f4e179b529de189322
Varun Srinivasan <varunsrin@gmail.com>
2023-07-26 10:00:23 -0700

    chore: separate helpers

--- test/IdRegistry/IdRegistry.symbolic.t.sol
+++ test/IdRegistry/IdRegistry.symbolic.t.sol
@@ -9,7 +9,6 @@ import {IdRegistryHarness} from "../Utils.sol";
 contract IdRegistrySymTest is SymTest, Test {
     IdRegistryHarness idRegistry;
     address trustedCaller;
-
     address x;
     address y;
 
@@ -37,43 +36,25 @@ contract IdRegistrySymTest is SymTest, Test {
         y = svm.createAddress("y");
     }
 
-    // Additional setup to cover various input states
-    function init() public {
-        if (svm.createBool("disableTrustedOnly?")) {
-            idRegistry.disableTrustedOnly();
-        }
-        if (svm.createBool("pauseRegistration?")) {
-            idRegistry.pauseRegistration();
-        }
-    }
-
-    // Verify the IdRegistry invariants
-    function check_invariant(bytes4 selector, address caller) public {
-        init();
-
-        // Consider two distinct addresses
+    function verifyInvariants(bytes4 selector, address caller) public {
+        _initState();
         vm.assume(x != y);
 
         // Record pre-state
         uint256 oldIdX = idRegistry.idOf(x);
         uint256 oldIdY = idRegistry.idOf(y);
-
         uint256 oldIdCounter = idRegistry.getIdCounter();
-
         bool oldPaused = idRegistry.paused();
 
         // Execute an arbitrary tx to IdRegistry
         vm.prank(caller);
-        (bool success,) = address(idRegistry).call(mk_calldata(selector));
+        (bool success,) = address(idRegistry).call(_calldataFor(selector));
         vm.assume(success); // ignore reverting cases
 
         // Record post-state
         uint256 newIdX = idRegistry.idOf(x);
         uint256 newIdY = idRegistry.idOf(y);
-
         uint256 newIdCounter = idRegistry.getIdCounter();
-
-        // Verify invariant properties
 
         // Ensure that there is no recovery address associated with fid 0.
         assert(idRegistry.getRecoveryOf(0) == address(0));
@@ -94,8 +75,88 @@ contract IdRegistrySymTest is SymTest, Test {
         }
     }
 
-    function mk_calldata(bytes4 selector) internal returns (bytes memory) {
-        // Generate calldata based on the function selector
+    function verifyTransfer(address caller, address to, address other) public {
+        _initState();
+        vm.assume(other != caller && other != to);
+
+        // Record pre-state
+        uint256 oldIdCaller = idRegistry.idOf(caller);
+        uint256 oldIdTo = idRegistry.idOf(to);
+        uint256 oldIdOther = idRegistry.idOf(other);
+
+        // Execute transfer with symbolic arguments
+        vm.prank(caller);
+        idRegistry.transfer(to, svm.createUint256("deadline"), svm.createBytes(65, "sig"));
+
+        // Record post-state
+        uint256 newIdCaller = idRegistry.idOf(caller);
+        uint256 newIdTo = idRegistry.idOf(to);
+        uint256 newIdOther = idRegistry.idOf(other);
+
+        // Ensure that the fid has been transferred from the `caller` to the `to`.
+        assert(newIdTo == oldIdCaller);
+        if (caller != to) {
+            assert(oldIdCaller != 0 && newIdCaller == 0);
+            assert(oldIdTo == 0 && newIdTo != 0);
+        }
+
+        // Ensure that the other fids are not affected.
+        assert(newIdOther == oldIdOther);
+    }
+
+    function verifyRecovery(address caller, address from, address to, address other) public {
+        _initState();
+        vm.assume(other != from && other != to);
+
+        // Record pre-state
+        uint256 oldIdFrom = idRegistry.idOf(from);
+        uint256 oldIdTo = idRegistry.idOf(to);
+        uint256 oldIdOther = idRegistry.idOf(other);
+        address oldRecoveryFrom = idRegistry.getRecoveryOf(oldIdFrom);
+
+        // Execute recover with symbolic arguments
+        vm.prank(caller);
+        idRegistry.recover(from, to, svm.createUint256("deadline"), svm.createBytes(65, "sig"));
+
+        // Record post-state
+        uint256 newIdFrom = idRegistry.idOf(from);
+        uint256 newIdTo = idRegistry.idOf(to);
+        uint256 newIdOther = idRegistry.idOf(other);
+
+        // Ensure that the caller is the recovery address
+        assert(caller == oldRecoveryFrom);
+
+        // Ensure that the fid has been transferred from the `from` to the `to`.
+        assert(newIdTo == oldIdFrom);
+        if (from != to) {
+            assert(oldIdFrom != 0 && newIdFrom == 0);
+            assert(oldIdTo == 0 && newIdTo != 0);
+        }
+
+        // Ensure that the other fids are not affected.
+        assert(newIdOther == oldIdOther);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Initialize IdRegistry with symbolic arguments for state.
+     */
+    function _initState() public {
+        if (svm.createBool("disableTrustedOnly?")) {
+            idRegistry.disableTrustedOnly();
+        }
+        if (svm.createBool("pauseRegistration?")) {
+            idRegistry.pauseRegistration();
+        }
+    }
+
+    /**
+     * @dev Generates valid calldata for a given function selector.
+     */
+    function _calldataFor(bytes4 selector) internal returns (bytes memory) {
         bytes memory args;
         if (selector == idRegistry.registerFor.selector) {
             args = abi.encode(
@@ -116,77 +177,7 @@ contract IdRegistrySymTest is SymTest, Test {
         } else {
             args = svm.createBytes(1024, "data");
         }
+
         return abi.encodePacked(selector, args);
-    }
-
-    function check_transfer(address caller, address to, address other) public {
-        init();
-
-        // Consider another address that is not involved
-        vm.assume(other != caller && other != to);
-
-        // Record pre-state
-        uint256 oldIdCaller = idRegistry.idOf(caller);
-        uint256 oldIdTo = idRegistry.idOf(to);
-        uint256 oldIdOther = idRegistry.idOf(other);
-
-        // Execute transfer with symbolic arguments
-        vm.prank(caller);
-        idRegistry.transfer(to, svm.createUint256("deadline"), svm.createBytes(65, "sig"));
-
-        // Record post-state
-        uint256 newIdCaller = idRegistry.idOf(caller);
-        uint256 newIdTo = idRegistry.idOf(to);
-        uint256 newIdOther = idRegistry.idOf(other);
-
-        // Verify correctness properties
-
-        // Ensure that the fid has been transferred from the `caller` to the `to`.
-        assert(newIdTo == oldIdCaller);
-        if (caller != to) {
-            assert(oldIdCaller != 0 && newIdCaller == 0);
-            assert(oldIdTo == 0 && newIdTo != 0);
-        }
-
-        // Ensure that the other fids are not affected.
-        assert(newIdOther == oldIdOther);
-    }
-
-    function check_recover(address caller, address from, address to, address other) public {
-        init();
-
-        // Consider other address that is not involved
-        vm.assume(other != from && other != to);
-
-        // Record pre-state
-        uint256 oldIdFrom = idRegistry.idOf(from);
-        uint256 oldIdTo = idRegistry.idOf(to);
-        uint256 oldIdOther = idRegistry.idOf(other);
-
-        address oldRecoveryFrom = idRegistry.getRecoveryOf(oldIdFrom);
-
-        // Execute recover with symbolic arguments
-        vm.prank(caller);
-        idRegistry.recover(from, to, svm.createUint256("deadline"), svm.createBytes(65, "sig"));
-
-        // Record post-state
-        uint256 newIdFrom = idRegistry.idOf(from);
-        uint256 newIdTo = idRegistry.idOf(to);
-        uint256 newIdOther = idRegistry.idOf(other);
-
-        // Verify correctness properties
-
-        // Ensure that the caller is the recovery address
-        assert(caller == oldRecoveryFrom);
-
-        // Ensure that the fid has been transferred from the `from` to the `to`.
-        assert(newIdTo == oldIdFrom);
-        if (from != to) {
-            assert(oldIdFrom != 0 && newIdFrom == 0);
-            assert(oldIdTo == 0 && newIdTo != 0);
-        }
-
-        // Ensure that the other fids are not affected.
-        assert(newIdOther == oldIdOther);
     }
 }
