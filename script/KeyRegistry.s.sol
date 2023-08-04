be8a43050775b8881ccd46b12e4f49e5d5fd8af9
horsefacts <webmaster@terminally.online>
2023-08-04 14:20:52 -0400

    chore: add .env.example defaults

--- script/KeyRegistry.s.sol
+++ script/KeyRegistry.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
+import {IdRegistry} from "../src/IdRegistry.sol";
 import {KeyRegistry} from "../src/KeyRegistry.sol";
 import {ImmutableCreate2Deployer} from "./lib/ImmutableCreate2Deployer.sol";
 
@@ -8,13 +9,15 @@ contract IdRegistryScript is ImmutableCreate2Deployer {
     uint24 internal constant KEY_REGISTRY_MIGRATION_GRACE_PERIOD = 1 days;
 
     function run() public {
-        address idRegistry = vm.envAddress("ID_REGISTRY_ADDRESS");
-        address initialOwner = vm.envAddress("KEY_REGISTRY_OWNER_ADDRESS");
+        address initialIdRegistryOwner = vm.envAddress("ID_REGISTRY_OWNER_ADDRESS");
+        address initialKeyRegistryOwner = vm.envAddress("KEY_REGISTRY_OWNER_ADDRESS");
+
+        address idRegistry = register("IdRegistry", type(IdRegistry).creationCode, abi.encode(initialIdRegistryOwner));
 
         register(
             "KeyRegistry",
             type(KeyRegistry).creationCode,
-            abi.encode(idRegistry, KEY_REGISTRY_MIGRATION_GRACE_PERIOD, initialOwner)
+            abi.encode(idRegistry, KEY_REGISTRY_MIGRATION_GRACE_PERIOD, initialKeyRegistryOwner)
         );
 
         deploy();
