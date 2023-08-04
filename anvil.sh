98abaef14f54d11a650792d9fb0612aa65bd9de1
horsefacts <webmaster@terminally.online>
2023-08-04 10:39:51 -0400

    chore: remove job control

--- anvil.sh
+++ anvil.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -me
+set -e
 
 PORT=8545
 HOST="0.0.0.0"
@@ -8,6 +8,7 @@ SLEEP_DURATION=0.1
 
 # Start the Anvil server in auto-impersonate mode.
 anvil --auto-impersonate --host "$HOST" --rpc-url "$MAINNET_RPC_URL" &
+ANVIL_PID=$!
 
 # Wait until Anvil is ready
 while ! nc -z localhost "$PORT"; do sleep "$SLEEP_DURATION"; done
@@ -15,5 +16,5 @@ while ! nc -z localhost "$PORT"; do sleep "$SLEEP_DURATION"; done
 # Deploy contracts, forking from the provided RPC URL
 forge script script/Deploy.s.sol --rpc-url "http://localhost:$PORT" --unlocked --broadcast --sender "$DEPLOYER"
 
-# Foreground Anvil
-fg %1
+# Wait for Anvil
+wait $ANVIL_PID
