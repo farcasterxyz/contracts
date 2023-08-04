a7321bd06bf5caae6185de9457cd10a458e0dcaf
horsefacts <webmaster@terminally.online>
2023-08-04 10:58:59 -0400

    chore: disable auto impersonation after deploy

--- anvil.sh
+++ anvil.sh
@@ -6,15 +6,21 @@ PORT=8545
 HOST="0.0.0.0"
 SLEEP_DURATION=0.1
 
-# Start the Anvil server in auto-impersonate mode.
-anvil --auto-impersonate --host "$HOST" --rpc-url "$MAINNET_RPC_URL" &
+# Start the Anvil server.
+anvil --host "$HOST" --rpc-url "$MAINNET_RPC_URL" &
 ANVIL_PID=$!
 
 # Wait until Anvil is ready
 while ! nc -z localhost "$PORT"; do sleep "$SLEEP_DURATION"; done
 
+# Enable auto-impersonation
+cast rpc anvil_autoImpersonateAccount true
+
 # Deploy contracts, forking from the provided RPC URL
 forge script script/Deploy.s.sol --rpc-url "http://localhost:$PORT" --unlocked --broadcast --sender "$DEPLOYER"
+
+# Disable auto-impersonation
+cast rpc anvil_autoImpersonateAccount false
 
 # Wait for Anvil
 wait $ANVIL_PID
