b1a2b85b52edfaba2024fde217e722b58171a05c
horsefacts <webmaster@terminally.online>
2023-08-21 21:01:40 -0400

    chore: update deployment parameters

--- test/Deploy/Deploy.t.sol
+++ test/Deploy/Deploy.t.sol
@@ -11,11 +11,11 @@ import {
     Bundler,
     IMetadataValidator
 } from "../../script/Deploy.s.sol";
+import "forge-std/console.sol";
 
 /* solhint-disable state-visibility */
 
-contract DeployTest is Test {
-    Deploy internal deploy;
+contract DeployTest is Deploy, Test {
     StorageRegistry internal storageRegistry;
     IdRegistry internal idRegistry;
     KeyRegistry internal keyRegistry;
@@ -37,13 +37,11 @@ contract DeployTest is Test {
     address internal app;
     uint256 internal appPk;
 
-    address internal owner = makeAddr("owner");
+    address internal deployer = address(this);
+    address internal alpha = makeAddr("alpha");
+    address internal beta = makeAddr("beta");
     address internal vault = makeAddr("vault");
-    address internal roleAdmin = makeAddr("roleAdmin");
-    address internal admin = makeAddr("admin");
-    address internal operator = makeAddr("operator");
-    address internal treasurer = makeAddr("treasurer");
-    address internal bundlerTrustedCaller = makeAddr("bundlerTrustedCaller");
+    address internal relayer = makeAddr("relayer");
 
     // @dev OP Mainnet ETH/USD price feed
     address internal priceFeed = address(0x13e3Ee699D1909E989722E753853AE30b17e08c5);
@@ -61,22 +59,23 @@ contract DeployTest is Test {
         (app, appPk) = makeAddrAndKey("app");
 
         Deploy.DeploymentParams memory params = Deploy.DeploymentParams({
-            initialIdRegistryOwner: owner,
-            initialKeyRegistryOwner: owner,
-            initialBundlerOwner: owner,
+            initialIdRegistryOwner: alpha,
+            initialKeyRegistryOwner: alpha,
+            initialBundlerOwner: alpha,
+            initialValidatorOwner: alpha,
             priceFeed: priceFeed,
             uptimeFeed: uptimeFeed,
             vault: vault,
-            roleAdmin: roleAdmin,
-            admin: admin,
-            operator: operator,
-            treasurer: treasurer,
-            bundlerTrustedCaller: bundlerTrustedCaller
+            roleAdmin: alpha,
+            admin: beta,
+            operator: relayer,
+            treasurer: relayer,
+            bundlerTrustedCaller: relayer,
+            deployer: deployer
         });
 
-        deploy = new Deploy();
-
-        Deploy.Contracts memory contracts = deploy.runDeploy(params);
+        Deploy.Contracts memory contracts = runDeploy(params, false);
+        runSetup(contracts, params, false);
 
         storageRegistry = contracts.storageRegistry;
         idRegistry = contracts.idRegistry;
@@ -89,36 +88,50 @@ contract DeployTest is Test {
         assertEq(address(storageRegistry.priceFeed()), priceFeed);
         assertEq(address(storageRegistry.uptimeFeed()), uptimeFeed);
         assertEq(storageRegistry.deprecationTimestamp(), block.timestamp + 365 days);
-        assertEq(storageRegistry.usdUnitPrice(), deploy.INITIAL_USD_UNIT_PRICE());
-        assertEq(storageRegistry.maxUnits(), deploy.INITIAL_MAX_UNITS());
-        assertEq(storageRegistry.priceFeedCacheDuration(), deploy.INITIAL_PRICE_FEED_CACHE_DURATION());
-        assertEq(storageRegistry.uptimeFeedGracePeriod(), deploy.INITIAL_UPTIME_FEED_GRACE_PERIOD());
+        assertEq(storageRegistry.usdUnitPrice(), INITIAL_USD_UNIT_PRICE);
+        assertEq(storageRegistry.maxUnits(), INITIAL_MAX_UNITS);
+        assertEq(storageRegistry.priceFeedCacheDuration(), INITIAL_PRICE_FEED_CACHE_DURATION);
+        assertEq(storageRegistry.uptimeFeedGracePeriod(), INITIAL_UPTIME_FEED_GRACE_PERIOD);
 
-        assertEq(idRegistry.owner(), owner);
+        assertEq(storageRegistry.getRoleMemberCount(bytes32(0)), 1);
+        assertEq(storageRegistry.hasRole(bytes32(0), deployer), false);
 
-        assertEq(keyRegistry.owner(), owner);
+        assertEq(storageRegistry.getRoleMemberCount(keccak256("OWNER_ROLE")), 1);
+        assertEq(storageRegistry.hasRole(keccak256("OWNER_ROLE"), deployer), false);
+        assertEq(storageRegistry.hasRole(keccak256("OWNER_ROLE"), beta), true);
+
+        assertEq(storageRegistry.getRoleMemberCount(keccak256("OPERATOR_ROLE")), 2);
+        assertEq(storageRegistry.hasRole(keccak256("OPERATOR_ROLE"), deployer), false);
+        assertEq(storageRegistry.hasRole(keccak256("OPERATOR_ROLE"), address(bundler)), true);
+        assertEq(storageRegistry.hasRole(keccak256("OPERATOR_ROLE"), relayer), true);
+
+        assertEq(storageRegistry.getRoleMemberCount(keccak256("TREASURER_ROLE")), 1);
+        assertEq(storageRegistry.hasRole(keccak256("TREASURER_ROLE"), deployer), false);
+        assertEq(storageRegistry.hasRole(keccak256("TREASURER_ROLE"), relayer), true);
+
+        assertEq(idRegistry.owner(), deployer);
+        assertEq(idRegistry.pendingOwner(), alpha);
+
+        assertEq(keyRegistry.owner(), deployer);
+        assertEq(keyRegistry.pendingOwner(), alpha);
         assertEq(address(keyRegistry.idRegistry()), address(idRegistry));
-        assertEq(keyRegistry.gracePeriod(), deploy.KEY_REGISTRY_MIGRATION_GRACE_PERIOD());
+        assertEq(keyRegistry.gracePeriod(), KEY_REGISTRY_MIGRATION_GRACE_PERIOD);
 
-        assertEq(validator.owner(), owner);
+        assertEq(validator.owner(), alpha);
         assertEq(address(validator.idRegistry()), address(idRegistry));
 
-        assertEq(bundler.owner(), owner);
+        assertEq(bundler.owner(), alpha);
         assertEq(address(bundler.idRegistry()), address(idRegistry));
         assertEq(address(bundler.storageRegistry()), address(storageRegistry));
         assertEq(address(bundler.keyRegistry()), address(keyRegistry));
-        assertEq(bundler.trustedCaller(), bundlerTrustedCaller);
+        assertEq(bundler.trustedCaller(), relayer);
     }
 
     function test_e2e() public {
-        vm.startPrank(owner);
-        idRegistry.setTrustedCaller(address(bundler));
-        keyRegistry.setTrustedCaller(address(bundler));
-        keyRegistry.setValidator(1, 1, IMetadataValidator(address(validator)));
+        vm.startPrank(alpha);
+        idRegistry.acceptOwnership();
+        keyRegistry.acceptOwnership();
         vm.stopPrank();
-
-        vm.prank(roleAdmin);
-        storageRegistry.grantRole(keccak256("OPERATOR_ROLE"), address(bundler));
 
         vm.prank(address(bundler));
         uint256 requestFid = idRegistry.trustedRegister(app, address(0));
@@ -137,11 +150,11 @@ contract DeployTest is Test {
 
         Bundler.SignerData[] memory signers = new Bundler.SignerData[](1);
         signers[0] = Bundler.SignerData({keyType: 1, key: key, metadataType: 1, metadata: metadata});
-        vm.prank(bundlerTrustedCaller);
+        vm.prank(relayer);
         bundler.trustedRegister(Bundler.UserData({to: alice, recovery: bob, signers: signers, units: 1}));
         assertEq(idRegistry.idOf(alice), 2);
 
-        vm.startPrank(owner);
+        vm.startPrank(alpha);
         idRegistry.disableTrustedOnly();
         keyRegistry.disableTrustedOnly();
         bundler.disableTrustedOnly();
