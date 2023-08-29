28b4079f060ebe878cbec49bb56f569d6b8ce1ae
horsefacts <109845214+horsefacts@users.noreply.github.com>
2023-08-28 19:05:53 -0700

    chore: update license identifiers (#382)

--- src/interfaces/IdRegistryLike.sol
+++ src/interfaces/IdRegistryLike.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
 interface IdRegistryLike {
     function idOf(address fidOwner) external view returns (uint256);
