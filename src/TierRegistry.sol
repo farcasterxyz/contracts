7b3d72e90bec3b5e3bbd7f69f8f8081ed5f6e7eb
Aditi Srinivasan <aditisri@mit.edu>
2025-06-02 16:27:16 -0400

    feat: add tier contract (#476)
    feat: add tier registry contract

---
+++ src/TierRegistry.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {Ownable2Step} from "openzeppelin/contracts/access/Ownable2Step.sol";
+import {Pausable} from "openzeppelin/contracts/security/Pausable.sol";
+import "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {ITierRegistry} from "./interfaces/ITierRegistry.sol";
+
+/**
+ * @title Farcaster TierRegistry
+ *
+ * @notice See https://github.com/farcasterxyz/contracts/blob/v3.1.0/docs/docs.md for an overview.
+ *
+ * @custom:security-contact security@merklemanufactory.com
+ */
+contract TierRegistry is ITierRegistry, Ownable2Step, Pausable {
+    using SafeERC20 for IERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    string public constant VERSION = "2025.06.01";
+
+    /*//////////////////////////////////////////////////////////////
+                                STORAGE 
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Maps tier IDs to their corresponding configuration data
+     *      including payment parameters, duration limits,
+     *      and activation status.
+     * @custom:param tierId The unique identifier for the tier
+     */
+    mapping(uint256 tierId => TierInfo info) internal _tierInfoByTier;
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    uint256 public nextTierId;
+
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Set the initial parameters and pause the contract.
+     *
+     * @param _initialOwner                  Initial owner address.
+     */
+    constructor(
+        address _initialOwner
+    ) {
+        _transferOwnership(_initialOwner);
+        _pause();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              VIEWS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function price(uint256 tier, uint256 forDays) external view returns (uint256) {
+        TierInfo memory info = _tierInfoByTier[tier];
+        if (!info.isActive) revert InvalidTier();
+        return info.tokenPricePerDay * forDays;
+    }
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function tierInfo(
+        uint256 tier
+    ) external view returns (TierInfo memory) {
+        return _tierInfoByTier[tier];
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        TIER PURCHASING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function purchaseTier(uint256 fid, uint256 tier, uint256 forDays) external whenNotPaused {
+        TierInfo memory info = _tierInfoByTier[tier];
+        if (forDays == 0) revert InvalidDuration();
+        if (!info.isActive) revert InvalidTier();
+        if (forDays < info.minDays) revert InvalidDuration();
+        if (forDays > info.maxDays) revert InvalidDuration();
+
+        uint256 cost = info.tokenPricePerDay * forDays;
+
+        emit PurchasedTier(fid, tier, forDays, msg.sender);
+
+        info.paymentToken.safeTransferFrom(msg.sender, info.vault, cost);
+    }
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function batchPurchaseTier(
+        uint256 tier,
+        uint256[] calldata fids,
+        uint256[] calldata forDays
+    ) external whenNotPaused {
+        if (fids.length == 0) revert InvalidBatchInput();
+        if (fids.length != forDays.length) revert InvalidBatchInput();
+
+        TierInfo memory info = _tierInfoByTier[tier];
+        if (!info.isActive) revert InvalidTier();
+
+        uint256 totalCost;
+        for (uint256 i; i < fids.length; ++i) {
+            uint256 numDays = forDays[i];
+            if (numDays == 0) revert InvalidDuration();
+            if (numDays < info.minDays) revert InvalidDuration();
+            if (numDays > info.maxDays) revert InvalidDuration();
+            totalCost += info.tokenPricePerDay * numDays;
+        }
+
+        for (uint256 i; i < fids.length; ++i) {
+            // The payer field in the event will point to a contract if the caller is a contract
+            emit PurchasedTier(fids[i], tier, forDays[i], msg.sender);
+        }
+
+        // Payment is made in a single transaction to save gas
+        info.paymentToken.safeTransferFrom(msg.sender, info.vault, totalCost);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         PERMISSIONED ACTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function setTier(
+        uint256 tier,
+        address paymentToken,
+        uint256 minDays,
+        uint256 maxDays,
+        uint256 tokenPricePerDay,
+        address vault
+    ) external onlyOwner {
+        if (paymentToken == address(0)) revert InvalidTokenAddress();
+        if (minDays == 0) revert InvalidDuration();
+        if (maxDays == 0) revert InvalidDuration();
+        if (minDays > maxDays) revert InvalidDuration();
+        if (tokenPricePerDay == 0) revert InvalidPrice();
+        if (vault == address(0)) revert InvalidVaultAddress();
+        if (tier > nextTierId) revert InvalidTier();
+
+        emit SetTier(tier, minDays, maxDays, vault, paymentToken, tokenPricePerDay);
+
+        _tierInfoByTier[tier] = TierInfo({
+            minDays: minDays,
+            maxDays: maxDays,
+            paymentToken: IERC20(paymentToken),
+            tokenPricePerDay: tokenPricePerDay,
+            vault: vault,
+            isActive: true
+        });
+
+        if (tier == nextTierId) {
+            nextTierId += 1;
+        }
+    }
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function deactivateTier(
+        uint256 tier
+    ) external onlyOwner {
+        if (!_tierInfoByTier[tier].isActive) revert InvalidTier();
+
+        emit DeactivateTier(tier);
+
+        _tierInfoByTier[tier].isActive = false;
+    }
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @inheritdoc ITierRegistry
+     */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
---
+++ test/TierRegistry/TierRegistryTestSuite.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {TestSuiteSetup} from "../TestSuiteSetup.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {TierRegistry} from "../../src/TierRegistry.sol";
+
+contract TestToken is ERC20 {
+    constructor(
+        address source
+    ) ERC20("TestToken", "TTK") {
+        _mint(source, 1_000_000_000_000);
+    }
+}
+
+abstract contract TierRegistryTestSuite is TestSuiteSetup {
+    TierRegistry tierRegistry;
+    TestToken public token;
+
+    address internal deployer = address(this);
+    address internal tokenSource = makeAddr("tokenSource");
+
+    uint256 internal immutable DEPLOYED_AT = block.timestamp + 3600;
+    uint256 public immutable DEFAULT_MIN_DAYS = 1;
+    uint256 public immutable DEFAULT_MAX_DAYS = type(uint64).max;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        token = new TestToken(tokenSource);
+
+        vm.warp(DEPLOYED_AT);
+
+        tierRegistry = new TierRegistry(owner);
+
+        vm.prank(owner);
+        tierRegistry.unpause();
+
+        addKnownContract(address(tierRegistry));
+        addKnownContract(address(token));
+    }
+}
